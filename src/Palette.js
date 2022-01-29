74e8b08c13bfd82365d1c4af6cced788d8a42b73
Bryan Bordeman <bryanbordeman@Bryans-MBP.home>
2022-01-29 07:03:36 -0500

    first commit

---
+++ src/Palette.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Palette extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <div className='Palette'>
+                {/* Navbar goes here */}
+                <div className='Palette-colors'>
+                    {/* Color boxes */}
+                </div>
+                {/* Footer goes here */}
+            </div>
+        );
+    }
+}
+ 
+export default Palette;
